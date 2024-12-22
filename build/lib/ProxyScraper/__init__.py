2236c317310bf9c650b6c726b3a7aa62f6fa3907
adjidev <153899174+AdjiDev@users.noreply.github.com>
2024-12-22 11:08:39 +0700

    Delete build directory

--- build/lib/ProxyScraper/__init__.py
+++
@@ -1,1 +0,0 @@
-from ._adjisan_scraper import GetProxy
--- build/lib/ProxyScraper/bin/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ProxyList import AllProxyUrl
-from .ProxyList import HttpProxy
-from .ProxyList import HttpsProxy
-from .ProxyList import Socks4Proxy
-from .ProxyList import Socks5Proxy
